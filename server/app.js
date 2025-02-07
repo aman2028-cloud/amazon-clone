91308576688705faf861c4f54fa18a96c0a54458
Aman <apahwa2005@gmail.com>
2025-02-07 18:17:20 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5007;
+const cookieParser = require("cookie-parser");
+const DefaultData = require("./defaultdata");
+require("./db/conn");
+const router = require("./routes/router");
+const products = require("./models/productsSchema");
+const jwt = require("jsonwebtoken");
+const path =require("path");
+
+
+// middleware
+app.use(express.json());
+app.use(cookieParser(""));
+
+app.use(router);
+// app.get("/",(req,res)=>{
+//     res.send("your server is running");
+// });
+
+
+// for deploy//
+const _dirname = path.resolve();
+
+app.use(express.static(path.join(_dirname,"/client/build")))
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"client","build","index.html"))
+})
+
+
+// if(process.env.NODE_ENV == "production"){
+//     app.use(express.static("client/build"));
+// }
+
+app.listen(port,()=>{
+    console.log(`your server is running on port ${port} `);
+});
+
+DefaultData();
