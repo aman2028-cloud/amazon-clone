d83c8f60e51e1c1ad82ff256583523acf12ff3f5
aman2028-cloud <apahwa2005@gmail.com>
2025-02-08 00:16:23 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DB = process.env.DATABASE;
+const DB = "mongodb+srv://new_user:aman_2020@cluster0.hhcpx.mongodb.net/";
 
 mongoose.connect(DB,{
     useUnifiedTopology:true,
