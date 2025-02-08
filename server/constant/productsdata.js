9d001ce4469ee0472515d23d17b5d67c4f55d2a5
aman2028-cloud <apahwa2005@gmail.com>
2025-02-08 09:47:11 +0530

    Update productsdata.js

--- server/constant/productsdata.js
+++ server/constant/productsdata.js
@@ -83,7 +83,61 @@ const products = [
         description: '',
         discount: 'From ₹499', 
         tagline: 'Kubra, Nova & more' 
-    }
+    },
+     
+        { 
+             id:"products6",
+            url: 'https://m.media-amazon.com/images/I/81tHOIXhseL.jpg',
+            detailUrl: 'https://m.media-amazon.com/images/I/81tHOIXhseL.jpg', 
+            title: {
+                shortTitle: 'REALME 12 Pro 5G',
+                longTitle: 'REALME 12 Pro 5G (Submarine Blue, 12GB RAM, 256GB Storage)'
+            }, 
+            price: {
+                mrp: 29989,
+                cost: 41124,
+                discount: '40%'
+            },
+            description: '(Submarine Blue, 12GB RAM, 256GB Storage) Upto 24GB(12+12) Dynamic RAM | Snapdragon 6 Gen 1 Processor | 50 MP OIS | 32 MP 2X Flagship Telephoto Camera | 120 Hz Curved Vision',
+            discount: 'From ₹4499', 
+            tagline: 'REALME 12 Pro 5G (Submarine Blue, 12GB RAM, 256GB Storage)' 
+        }
+        ,
+        { 
+             id:"products7",
+            url: 'https://m.media-amazon.com/images/I/512boAL7vuL._SY879_.jpg',
+            detailUrl: 'https://m.media-amazon.com/images/I/512boAL7vuL._SY879_.jpg', 
+            title: {
+                shortTitle: 'LAMBOO Men ',
+                longTitle: 'Printed Lining Print Cotton Lycra Slim Fit Cut Away Collar Casual Shirt'
+            }, 
+            price: {
+                mrp: 439,
+                cost: 899,
+                discount: '40%'
+            },
+            description: 'Printed Lining Print Cotton Lycra Slim Fit Cut Away Collar Casual Shirt',
+            discount: 'From ₹199', 
+            tagline: 'Printed Lining Print Cotton Lycra Slim Fit Cut Away Collar Casual Shirt' 
+        },
+        { 
+                id:"products8",
+               url: 'https://m.media-amazon.com/images/I/41sMtOdJdUL.jpg', 
+               detailUrl: 'https://m.media-amazon.com/images/I/41sMtOdJdUL.jpg', 
+               title: {
+                   shortTitle: 'Kashis CreationsFloral Print Rayon Kurta for Women',
+                   longTitle: '| Regular Fit Top | Stylish Top for Girls | Top for Office | Kurti for Women | Black |'
+               }, 
+               price: {
+                   mrp: 310,
+                   cost: 166,
+                   discount: '50%'
+               },
+               description: 'If you are looking for : women top for women White top women crop top women top stylish crop top women women stylish top crop top women women stylish top women s top women western top sleeveless top women women top dress 3/4th sleeve top women jaipuri women top floral top office top then we are here for you.'
+            ,
+               discount: 'Upto 70% Off', 
+               tagline: 'Deal of the Day' 
+            }
 ];
 
 module.exports = products;
